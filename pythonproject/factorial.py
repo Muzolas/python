8194812fe22338eb5be16cc2b5b33cab1b760464
Muzaffer Beysan Kalem <mbk95mbk95@gmail.com>
2022-09-18 15:49:36 +0300

    .

---
+++ pythonproject/factorial.py
@@ -0,0 +1,18 @@
+print("-" * 50)
+print("\t  Factorial Discovery Program \n\tPress '-1' to exit the program.")
+print("-" * 50)
+
+while True:
+    number = int(input("\nEnter the number to be factored into: "))
+
+    if(number == -1):
+        print("Program Termination...")
+        break
+
+    else:
+        factorial = 1
+        for i in range(2,number+1):
+            factorial *= i
+    
+    print("\n{}! = {}".format(number,factorial))
+    
