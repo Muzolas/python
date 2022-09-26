d69ac4d69bce7dacc02b0248b0f7598a7982bc56
Muzaffer Beysan Kalem <mbk95mbk95@gmail.com>
2022-09-27 00:47:17 +0300

    .

---
+++ pythonproject/sudoku/sudoku.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+main=tk.Tk()
+
+main.title('Sudoku!')
+label = tk.Label(main,text = '➤SUDOKU!',font='Times 20 bold')
+main.state('zoomed')
+label.pack()
+main.resizable(False,False)
+
+def start():
+    pass
+
+def clear():
+    pass
+
+start=tk.Button(main,text='Start!',font='Helvetica 15 bold',fg='White',bg='Green',command = start)
+start.place(x=750,y=1000)
+
+clear=tk.Button(main,text='Clear',font='Helvetica 15 bold',fg='White',bg='Red',command = clear)
+clear.place(x=1100,y=1000)
+
+name=tk.Entry(main,font='Helvetica 15 bold')
+name.place(x=850,y=1000,height='40')
+
+main.mainloop()
